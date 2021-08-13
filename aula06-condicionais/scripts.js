7befc73ec28bf7c073f658dafb900aa59e257c5b
Gleydsonilo Negre <83300802+gleydsonilo@users.noreply.github.com>
2021-08-13 11:30:07 -0300

    Update scripts.js
    Ajustes na descrição do exercício 06.

--- aula06-condicionais/scripts.js
+++ aula06-condicionais/scripts.js
@@ -1,15 +1,16 @@
 /*
 Exercício:
-O IMC significa Índice de Massa Corpórea e é um indicador utilizado pela OMS (Organização Mundial da Saúde) como referência para saber a saúde de uma pessoa com base no seu peso e altura.
+O IMC significa Índice de Massa Corpórea e é um indicador utilizado pela OMS (Organização Mundial da Saúde) como referência para saber a saúde de uma pessoa com 
+base no seu peso e altura.
 
 O cálculo do IMC é feito pela divisão do peso de alguém pela sua altura ao quadrado. Os valores de referência do índice são:
 
         IMC	            Classificação	 Grau de Obesidade
-Menor do que 18,50	       Magreza	            0
-Entre 18,50 e 24,99	       Normal              	0
-Entre 25,00 e 29,99	      Sobrepeso	            1
-Entre 30,00 e 39,99	      Obesidade	            2
-Maior ou igual a 40,00	Obesidade grave 	    3
+Menor do que 18,50	       Magreza	                0
+Entre 18,50 e 24,99	       Normal                   0
+Entre 25,00 e 29,99	       Sobrepeso	        1
+Entre 30,00 e 39,99	       Obesidade	        2
+Maior ou igual a 40,00	    Obesidade grave 	        3
 
 Faça um algoritmo que efetue o cálculo do IMC, com base nas variáveis peso e altura.
 
