e98a7d8b6c6eb1091de773b9f7f8c4dce72194f8
Gleydsonilo Negre <gleydsontaichou@gmail.com>
2021-08-13 08:29:25 -0300

    Exercício aula 04

---
+++ aula04-variaveis/scripts.js
@@ -0,0 +1,24 @@
+let nomeCompleto = "Gleydsonilo"
+let apelido = "Gleydson"
+let idade = 30
+let dataNascimento = "06/06/1991"
+let localNascimento = "Brasília - DF"
+let altura = 1.9
+let trabalhando = true
+
+apresentacao = "Meu nome é " 
++ nomeCompleto 
++ " (sou conhecido como " 
++ apelido 
++ ") e tenho " + idade 
++ " anos. Nasci no dia " 
++ dataNascimento 
++ ", na cidade de " 
++ localNascimento 
++ ". Tenho " 
++ altura 
++ "m de altura e atualmente estou " 
++ (trabalhando ? "empregado" : "desempregado") 
++ "."
+
+console.log(apresentacao)
